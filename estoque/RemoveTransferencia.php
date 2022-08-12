f8d4942e633e3b28cfbe251cd3260d5e5974bb23
Kaynnan Lemes <53870822+kaynnan@users.noreply.github.com>
2022-08-12 16:58:39 -0300

    Ajustes Array - Remove Transferência

--- estoque/RemoveTransferencia.php
+++ estoque/RemoveTransferencia.php
@@ -1,8 +1,16 @@
 <?php
 
-$idProduto = $_GET['idProduto'];
+$idProduto = $_GET['idProduto'] . " ";
+session_start();
 
-unset($_SESSION['transferencia'][11]);
+foreach ($_SESSION['transferencia'] as $key => $value) {
+    if (in_array("$idProduto", $_SESSION['transferencia'][$key])) {
+        // echo "Key exists!";
+        $valorAtual = $key;
+    } else {
+        //   echo "Key does not exist!";
+    }
+}
 
-
+unset($_SESSION['transferencia'][$valorAtual]);
 echo "<script>alert('Item excluido com sucesso!');window.location='FinalizarTransferencia.php'</script>";
