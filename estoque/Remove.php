f8f076279a7923e3201ec26e78f336ddf1e5a548
Bruno Soares Marcondes <51133906+BrunoUemg@users.noreply.github.com>
2023-01-09 09:41:17 -0300

    ajuste remove produto saida

--- estoque/Remove.php
+++ estoque/Remove.php
@@ -1,11 +1,27 @@
 <?php
 
-$idProduto =$_GET['idProduto'];
+// $idProduto =$_GET['idProduto'];
 
+// session_start();
+
+// unset($_SESSION['carrinho'][$idProduto]);
+
+// echo "<script>alert('Item excluido com sucesso!');window.location='Carrinho.php'</script>";
+
+$idProduto = $_GET['idProduto'] . " ";
 session_start();
 
-unset($_SESSION['carrinho'][$idProduto]);
+foreach ($_SESSION['carrinho'] as $key => $value) {
+    if (in_array("$idProduto", $_SESSION['carrinho'][$key])) {
+        // echo "Key exists!";
+        $valorAtual = $key;
+    } else {
+        //   echo "Key does not exist!";
+    }
+}
 
+unset($_SESSION['carrinho'][$valorAtual]);
 echo "<script>alert('Item excluido com sucesso!');window.location='Carrinho.php'</script>";
+
 
 ?>
